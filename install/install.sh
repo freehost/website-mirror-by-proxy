769c8e4d8cd1eec0115a26d9a24eb0e190442ac3
Ubuntu <ubuntu@website-mirror-by-proxy-1>
2014-12-03 09:01:59 +0000

    bug if using on host without path

--- install/install.sh
+++ install/install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+function pecl_install {
+	pecl install $1
+	echo "extension=$2.so" | sudo tee /etc/php5/mods-available/$2.ini
+	php5enmod $2
+}
+
 if [ $(/usr/bin/id -u) -ne 0 ]; then
     echo "Not running as root"
     exit
 fi
 
-echo "Installing general dependencies"
+echo "Installing Apache and PHP"
 apt-get install apache2 php5
 
-echo "Installing PHP cURL extension"
-apt-get install curl libcurl3 libcurl3-dev php5-curl
+echo "Installing HTTP extension"
+apt-get install libpcre3-dev libcurl3-openssl-dev php5-dev php-http php5-mcrypt php-pear
+pecl_install pecl/raphf raphf
+pecl_install pecl/propro propro
+pecl_install pecl_http-1.7.6 http
 
 echo "Enabling Apache modules"
 a2enmod rewrite
